57c62961c8334a7497a4df18da966cf01c23058d
kdorheim <kalyn.dorheim@pnnl.gov>
2025-08-29 10:44:37 -0400

    repeat_for_scns

--- R/helper.R
+++ R/helper.R
@@ -16,6 +16,40 @@ req_check <- function(x, req){
 }
 
 
+#' Replicate data over scenarios
+#'
+#' This is used to replicate default of preGCAM emissions which are needed
+#' to complete scenarios.
+#'
+#' @param x data frame containing the information that needs to be replicated over n number of scenarios
+#' @param scns vector of the scenarios
+#' @return data frame of results now containing scenario information
+#' @noRd
+repeat_for_scns <- function(x, scns){
+
+    # There data frame should not contain any scenario information,
+    # since that is what we are adding...
+    stopifnot(!"scenario" %in% names(x))
+
+    out <- data.frame()
+
+    # Make a copy of the data frame for each scenario.
+    for(i in seq_along(scns)){
+        out <- rbind(out, x)
+    }
+
+    # Add the scenario information to the data frame
+    scenario_vector <- rep(scns, each = nrow(out)/length(scns))
+    out$scenario <- scenario_vector
+
+
+    return(out)
+
+}
+
+
+
+
 
 
 
--- tests/testthat/test-helper.R
+++ tests/testthat/test-helper.R
@@ -14,3 +14,15 @@ test_that("req_check works", {
     expect_error(req_check(names(mtcars), "fake"), regexp = "x is misisng: fake")
 
 })
+
+test_that("repeat_for_scns works", {
+
+    scns <- c("fake1", "test2")
+    out <- repeat_for_scns(x = mtcars, scns = scns)
+
+    expect_equal(nrow(out), nrow(mtcars) * length(scns))
+    expect_equal(ncol(out), ncol(mtcars) + 1)
+    expect_true(req_check(x = names(out), req = "scenario"))
+    expect_error(req_check(x = names(mtcars), req = "scenario"))
+
+})
