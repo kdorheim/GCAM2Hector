cbfe10c3a39b4a06e950008a287a515a9ebb12bd
kdorheim <kalyn.dorheim@pnnl.gov>
2025-08-31 08:09:24 -0400

    internal get emiss functions

--- R/emiss.R
+++ R/emiss.R
@@ -1,10 +1,3 @@
-# Get the historical emissions used to drive Hector before GCAM emissions
-# are supplied to the Hector core.
-# Args
-#   file: path to the gcam emissions file, default is the data included in the auxiliary data
-# Returns: data.frame of the emissions used to drive Hector's historical period
-
-
 #' Get the historical emissions used to drive Hector before GCAM emissions
 #'
 #' Get the historical emissions used to drive Hector before GCAM emissions
@@ -106,3 +99,110 @@ get_nonCO2_emiss <- function(prjdata, gcam_emiss_file = NULL){
 
     return(out)
 }
+
+# TODO delte this but this may be helpful during dev and testing...
+# write.csv(df, "neg_co2_emiss.csv", row.names = FALSE)
+# df <- read.csv("neg_co2_emiss.csv")
+
+
+#' Change net CO2 emissions to strictly positive emissions and uptake
+#'
+#' This helper functions takes the CO2 emissions from FFI and LUC and
+#' changes them to the respective strictly positive values.
+#'
+#' @param df long data frame of the luc and ffi emissions
+#' @returns long data frame of the luc & ffi emissions and uptake, strictly positive.
+#' @import dplyr
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_wider
+#' @noRd
+handle_neg_CO2_emiss <- function(df){
+
+    req_check(names(df),  c("year", "value", "variable", "units", "scenario"))
+    req_check(df$variable,  c(LUC_EMISSIONS(), FFI_EMISSIONS()))
+
+    df %>%
+        tidyr::pivot_wider(names_from = variable, values_from = value) %>%
+        # Move the "negative emissions" to the uptake variables.
+        mutate(daccs_uptake = ifelse(ffi_emissions <= 0, -1 * ffi_emissions, 0),
+               luc_uptake = ifelse(luc_emissions <= 0, -1 * luc_emissions, 0)) %>%
+        # Now replace the "negative emissions" with the 0 value.
+        mutate(ffi_emissions = ifelse(ffi_emissions <= 0, 0, ffi_emissions),
+               luc_emissions = ifelse(luc_emissions <= 0, 0, luc_emissions)) %>%
+        tidyr::pivot_longer(cols = 4:7, names_to = "variable", values_to = "value") ->
+        out
+
+    return(out)
+
+
+}
+
+#' Get the the CO2 emissions from an GCAM output database
+#'
+#' @param prjdata path to the GCAM project file
+#' @param name path to the gcam emissions table otherwise will use internal data
+#' @return data.frame of the CO2 emissions that can be used as inputs to hector
+#' @import dplyr
+#' @import hector
+#' @noRd
+get_CO2_emiss <- function(prjdata, gcam_emiss_file = NULL){
+
+    # Extract the two CO2 queries!
+    query <- "CO2 emissions by region"
+    ffi   <- getQuery(prjdata, query = query)
+
+
+    # Get the fug CO2 emissions
+    # TODO this should be addressed at the query level
+    getQuery(prjdata, query = "nonCO2 emissions by region") %>%
+        filter(ghg == "CO2_FUG") %>%
+        mutate(ghg = "CO2") %>%
+        bind_rows(ffi) %>%
+        summarise(value = sum(value), .by = c("Units", "scenario", "ghg", "year")) ->
+        ffi
+
+    query <- "luc_emissions"
+    luc   <- getQuery(prjdata, query = query)
+    luc$variable <- query
+    luc$ghg <- query
+
+    # Map the GCAM emissions to Hector variable names and
+    # convert to Hector units.
+    ffi %>%
+        bind_rows(luc) %>%
+        left_join(EMISS_MAP_DF, by = join_by(ghg)) %>%
+        mutate(value = value * unit.conv) %>%
+        dplyr::summarise(value = sum(value),
+                         .by = c("scenario", "hector.units", "scenario", "year", "hector.name")) %>%
+        select(scenario, year, value, variable = hector.name, units = hector.units) %>%
+        # The future emissions start after the transition date.
+        filter(year > TRANSITION_DATE) ->
+        df
+
+    # Use the FFI and LUC emissions from GCAM to determine
+    # co2 emissions and uptake.
+    incomplete_future_emiss <- handle_neg_CO2_emiss(df = df)
+
+    # Get the historical emissions aka the emissions Hector should
+    # use before switching over to the GCAM emissions.
+    get_pregcam_emiss(file = gcam_emiss_file) %>%
+        filter(year <= TRANSITION_DATE) %>%
+        filter(variable %in% c(LUC_EMISSIONS(), LUC_UPTAKE(),
+                               FFI_EMISSIONS(), DACCS_UPTAKE())) %>%
+        repeat_for_scns(scns = unique(incomplete_future_emiss$scenario)) ->
+        pre_gcam_emiss
+
+
+    # Combine the historical emissions with the GCAM emissions,
+    # fill in the missing years.
+    pre_gcam_emiss %>%
+        rbind(incomplete_future_emiss) %>%
+        add_missing_yrs(req_years = 1750:2100)->
+        out
+
+    return(out)
+
+}
+
+
+
--- tests/testthat/test-emiss.R
+++ tests/testthat/test-emiss.R
@@ -1,3 +1,15 @@
+
+
+# TODO there should be a better way to handle this!
+if(basename(getwd()) == "testthat") {
+    base_dir <- "."
+} else {
+    base_dir <- file.path("tests", "testthat")
+}
+
+prj_data <- loadProject(file.path(base_dir, "gcam_db.dat"))
+
+
 test_that("get_pregcam_emiss works", {
 
     expect_true(is.data.frame(PREGCAM_EMISS_DF))
@@ -11,7 +23,7 @@ test_that("get_pregcam_emiss works", {
 
 })
 
-test_that("get_default_emiss works", {
+test_that("get_default_emiss", {
 
     expect_true(is.data.frame(DEFAULT_EMISS_DF))
 
@@ -27,24 +39,72 @@ test_that("get_default_emiss works", {
 
 test_that("get_nonCO2_emiss", {
 
-    # TODO there should be a better way to handle this!
-    if(basename(getwd()) == "testthat") {
-        base_dir <- "."
-    } else {
-        base_dir <- file.path("tests", "testthat")
-    }
-
-    prj_data <- loadProject(file.path(base_dir, "gcam_db.dat"))
     out <- get_nonCO2_emiss(prj_data)
 
     # All of the emissions other than the CO2 ones should be returned
     # here. Make sure that the correct variables are being returned a
-    # sand thaty we are not missing any expected ones...
+    # sand that we are not missing any expected ones...
     HECTOR_INPUTS <- c(GCAM_EMISS, DEFAULT_EMISS)
     co2_emiss <- c(LUC_EMISSIONS(), LUC_UPTAKE(), FFI_EMISSIONS(), DACCS_UPTAKE())
     expect_true(all(!out$variable %in% co2_emiss))
 
     non_co2_emiss <- setdiff(HECTOR_INPUTS, co2_emiss)
     expect_true(all(out$variable %in% non_co2_emiss))
+
+})
+
+
+test_that("handle_neg_CO2_emiss", {
+
+    # Make example data frames, one that is strictly positive and another
+    # that has a mix of negative values.
+    yrs <- 5
+    postive <- data.frame(scenario = "x",
+                          year = rep(seq(2020, by = 5, length.out = yrs), 2),
+                          variable = rep(c(LUC_EMISSIONS(), FFI_EMISSIONS()),  each = yrs),
+                          value = sample(x = c(1:100), size = yrs * 2, replace = TRUE),
+                          units = "fake")
+    mix <- postive
+    mix$value[4] <-  mix$value[4] * -1
+    mix$value[9] <-  mix$value[9] * -1
+    mix$value[10] <-  mix$value[10] * -1
+
+    # If there are no negative emissions then we would
+    # expect the uptake variables to equal 0.
+    out1 <- handle_neg_CO2_emiss(postive)
+
+    # There should be no change in the FFI and LUC emissions
+    expect_equal({out1 %>%
+            filter(variable %in% c(LUC_EMISSIONS(), FFI_EMISSIONS())) %>%
+            pull(value) %>%
+            sum}, sum(postive$value))
+
+    # The uptake emissions should all be 0
+    expect_true({all(out1 %>%
+                         filter(variable %in% c(LUC_UPTAKE(), DACCS_UPTAKE())) %>%
+                         pull(value) == 0)})
+
+
+
+    out2 <- handle_neg_CO2_emiss(mix)
+    # All of the values should be positive!
+    expect_true(all(out2$value >= 0))
+
+
+    # The total absolute values should be equal
+    expect_equal(sum(out2$value), sum(abs(mix$value)))
+
+
+})
+
+
+test_that("get_CO2_emiss", {
+
+
+    out1 <- get_CO2_emiss(prj_data)
+    expect_true(req_check(out1$year, 1745:2100))
+
+    co2_emiss <- c(LUC_EMISSIONS(), LUC_UPTAKE(), FFI_EMISSIONS(), DACCS_UPTAKE())
+    expect_true(req_check(out1$variable, co2_emiss))
 
 })
