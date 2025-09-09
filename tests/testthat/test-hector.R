e83e2ed6976bbeb213503b2137c188718e008f1e
kdorheim <kalyn.dorheim@pnnl.gov>
2025-09-09 12:34:23 -0400

    check the full workflow

--- tests/testthat/test-hector.R
+++ tests/testthat/test-hector.R
@@ -1,69 +1,50 @@
-# TODO: figure out a better way to suppress tests for the sake of time.
-TEST <- FALSE
-if(TEST){
-
-    test_that("internal.single_hector_run", {
-
-        # use a default scenario inputs to make sure
-        # we can run hector
-        ini <- system.file(package = "hector", "input/hector_ssp245.ini")
-        hc  <- newcore(ini)
-
-        system.file(package = "hector",
-                    "input/tables/ssp434_emiss-constraints_rf.csv") %>%
-            read_hector_csv %>%
-            filter(variable %in% c(GCAM_EMISS, DEFAULT_EMISS)) %>%
-            mutate(scenario = "fake") ->
-            inputs
-
-        out <- internal.single_hector_run(hc, inputs)
-        expect_true(is.data.frame(out))
-
-        # default run that should have different results
-        hc  <- newcore(ini)
-        run(hc, runtodate = 2100)
-        default_out <- fetchvars(hc, dates = 2025:2100, vars = out$variable)
-
-        default_out %>%
-            select(year, variable, default = value) %>%
-            left_join(out, by = join_by(year, variable)) %>%
-            mutate(SE = (default - value)^2) %>%
-            summarise(MSE = mean(SE), .by = c("variable")) ->
-            MSE_df
-
-        expect_true(all(MSE_df$MSE > 1e-8))
-
-
-
-
-    })
-
-}
-
-# TODO there should be a better way to handle this!
+# TODO there is probably a better way to handle but for now allow
+# tests to be launched from different env.
 if(basename(getwd()) == "testthat") {
     base_dir <- "."
 } else {
     base_dir <- file.path("tests", "testthat")
 }
-prj_file <- file.path(base_dir, "gcam_db.dat")
+prj_file <- file.path(base_dir, "prj_data.dat")
 prj_data <- loadProject(prj_file)
 
-test_that("run_GCAM2hector", {
 
-    db_dir <- "fake_dir"
-    db_name <- "fake_basexdb"
+test_that("internal.single_hector_run", {
 
-    # This should run with a single scenario.
-    out <- run_GCAM2hector(db_dir = db_dir,
-                           db_name = db_name,
-                           prj_file = prj_file)
+    # use a default scenario inputs to make sure
+    # we can run hector
+    ini <- system.file(package = "hector", "input/hector_ssp245.ini")
+    hc  <- newcore(ini)
 
+    system.file(package = "hector",
+                "input/tables/ssp434_emiss-constraints_rf.csv") %>%
+        read_hector_csv %>%
+        filter(variable %in% c(GCAM_EMISS, DEFAULT_EMISS)) %>%
+        mutate(scenario = "fake") ->
+        inputs
+
+    out <- internal.single_hector_run(hc, inputs)
     expect_true(is.data.frame(out))
 
-    # TODO add a test to see if it works with mulitple sceanrios...
+    # default run that should have different results
+    hc  <- newcore(ini)
+    run(hc, runtodate = 2100)
+    default_out <- fetchvars(hc, dates = 2025:2100, vars = out$variable)
+
+    default_out %>%
+        select(year, variable, default = value) %>%
+        left_join(out, by = join_by(year, variable)) %>%
+        mutate(SE = (default - value)^2) %>%
+        summarise(MSE = mean(SE), .by = c("variable")) ->
+        MSE_df
+
+    expect_true(all(MSE_df$MSE > 1e-8))
+
+
+
 
 })
+
 
 # test_that("internal.write_hector_csv and internal.write_ini", {
 #     # TODO need to add!
--- tests/testthat/test-rgcam.R
+++ tests/testthat/test-rgcam.R
@@ -7,15 +7,18 @@ if(FALSE){
         # There is probably a better way to do this but for now
         # let the code run with
         if(basename(getwd()) == "testthat") {
+            output_dir <- file.path("..", "..")
             base_dir <- "."
         } else {
-            base_dir <- file.path("..", "..")
+            output_dir <- "."
+            base_dir   <- file.path("tests", "testthat")
         }
 
-        db_dir <- file.path(base_dir, "gcam_output")
-        db_name <- file.path(base_dir, "database_basexdb")
+        db_dir   <- file.path(output_dir, "gcam_output")
+        db_name  <- file.path(output_dir, "database_basexdb")
+        prj_file <- file.path(base_dir, "prj_data.dat")
 
-        x <- get_all_queries(db_dir = db_dir, db_name = db_name)
+        x <- get_all_queries(db_dir = db_dir, db_name = db_name, prj_file = prj_file)
         expect_true(file.exists(x))
 
         expect_true(is.list(loadProject(x)))
@@ -26,8 +29,6 @@ if(FALSE){
                                              db_name = db_name,
                                              prj_file = "prj_data.dat",
                                              query_file = "fake.xml")})
-
-        file.remove(x)
 
     })
 }
@@ -41,10 +42,8 @@ test_that("fetch_GCAM_vs_hector works", {
     }
 
     # Use the example data
-    file <- file.path(base_dir, "gcam_db.dat")
+    file <- file.path(base_dir, "prj_data.dat")
     expect_true(file.exists(file))
-
-
 
 })
 
